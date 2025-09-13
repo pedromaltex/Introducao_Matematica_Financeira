bebac14159d205c798bd69dc46c9e0d8a80302f7
pedromaltex <pedromaltez16.1@gmail.com>
2025-09-13 10:25:41 +0100

    All Working Well

--- cap1_paul_wilmott/compound_interest/app.py
+++ cap1_paul_wilmott/compound_interest/app.py
@@ -18,61 +18,71 @@ with st.sidebar.form("params_form"):
     freq_contribuitions = st.radio("Frequency of Contributions:",
                                             ["Yearly", "Monthly", "Weekly"])
 
-    r = st.number_input("Interest Rate")
+    r = st.number_input("Interest Rate %")
     freq_r = st.radio("Frequency of Interest Rate:",
                             ["Yearly", "Monthly", "Weekly"])
 
     n_years = st.slider("Nº of Years", 1, 40, 1)
     submit = st.form_submit_button("Calculate")
 
-# --- Simulação ---
-fig = go.Figure()
+if submit:
+    # --- Simulação ---
+    fig = go.Figure()
 
-if freq_r == "Yearly":
-    r = r/100
-elif freq_r == "Monthly":
-    r /= 12 *100
-else:
-    r /= 52 *100
-
-
-if freq_contribuitions == "Yearly":
-    m_contribuitions = 1 # Number of Years in a Year
-elif freq_contribuitions == "Monthly":
-    m_contribuitions = 12 # Number of Months in a Year
-else:
-    m_contribuitions = 52 # Number of Weeks in a Year
-
-x_years = np.arange(1, n_years+1, 1)
-y = np.zeros(len(x_years))
-
-if contribuitions == 0:
-    for year in x_years:
-        y[year-1] = compounded_f_value(i_value, r, year)
-
-elif contribuitions > 0:
-    for year in x_years:
-        y[year-1] = compounded_f_value(i_value, r, year) + \
-        compounded_periodic_fvalue(contribuitions, r, m_contribuitions, year)
-else:
-    st.error('Error. Contributions lower than 0.', icon="🚨")
+    if freq_r == "Yearly":
+        print(freq_r)
+        anual_r = r / 100
+    elif freq_r == "Monthly":
+        anual_r = r*12 / 100
+    else:
+        anual_r = r*52 / 100
 
 
+    if freq_contribuitions == "Yearly":
+        m_contribuitions = 1 # Number of Years in a Year
+    elif freq_contribuitions == "Monthly":
+        m_contribuitions = 12 # Number of Months in a Year
+    else:
+        m_contribuitions = 52 # Number of Weeks in a Year
 
-fig.add_trace(go.Scatter(
-    x=x_years,
-    y=y,
-    mode='lines',
-    name=f"Acumulated Value",
-    opacity=0.7
-))
+    x_years = np.arange(1, n_years+1, 1)
+    y = np.zeros(len(x_years))
 
-fig.update_layout(
-    title=f"Final Portfolio Value",
-    xaxis_title="Steps",
-    yaxis_title="Value",
-    legend_title="Trajectory"
-)
+    if contribuitions == 0:
+        for year in x_years:
+            y[year-1] = compounded_f_value(i_value, anual_r, year)
 
-st.plotly_chart(fig)
+    elif contribuitions > 0:
+        for year in x_years:
+            y[year-1] = compounded_f_value(i_value, anual_r, year) + \
+            compounded_periodic_fvalue(contribuitions, anual_r, m_contribuitions, year)
+    else:
+        st.error('Error. Contributions lower than 0.', icon="🚨")
+
+
+
+    fig.add_trace(go.Scatter(
+        x=x_years,
+        y=y,
+        mode='lines',
+        name=f"Acumulated Value",
+        opacity=0.7
+    ))
+
+    fig.add_trace(go.Bar(
+        x=x_years,
+        y=y,
+        name=f"Accumulated Value",
+        opacity=0.7
+    ))
+
+
+    fig.update_layout(
+        title=f"Final Portfolio Value: {y[-1]:.2f}",
+        xaxis_title="Steps",
+        yaxis_title="Value",
+        legend_title="Trajectory"
+    )
+
+    st.plotly_chart(fig)
 
